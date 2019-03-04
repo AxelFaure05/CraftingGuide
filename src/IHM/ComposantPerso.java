e00efd43abb2f84a0aeb6e46fb33266b754ff824
Sylvain81452 <47383150+Sylvain81452@users.noreply.github.com>
2019-03-04 16:37:45 +0100

    Ajout fichiers de base de l'IHM en JavaFX
    +application.css
    +ComposantPerso.java
    +Main.java

---
+++ src/IHM/ComposantPerso.java
@@ -0,0 +1,10 @@
+package IHM;
+
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+
+public class ComposantPerso extends Parent{
+	public ComposantPerso() {
+		Button b = new Button();
+	}
+}
---
+++ src/IHM/Main.java
@@ -0,0 +1,36 @@
+package IHM;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//BorderPane root = new BorderPane();
+			Group root = new Group();
+			Group g2 = new Group();
+			
+			ComposantPerso c = new ComposantPerso();
+			
+			root.getChildren().addAll(g2,c);
+			
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("Crafting Guide");
+			primaryStage.setResizable(false);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
