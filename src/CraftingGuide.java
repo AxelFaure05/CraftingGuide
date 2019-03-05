1048fc9f624ca9c8abe299e1fe66c1090e05b8ee
Axel <akatsumero@outlook.fr>
2019-03-05 20:29:43 +0100

    Cette classe représente l'ensemble du projet, tout est initialisé dans celle-ci, notamment pour le JFX (J'imagine qu'elle soit représenter le Stage)

--- src/CraftingGuide.java
+++ src/CraftingGuide.java
@@ -1,11 +1,20 @@
+import java.util.Iterator;
+
 import Controller.*;
 import IHM.*;
 import Modele.*;
+import Modele.Composants.Item;
 
 public class CraftingGuide {
 	
 	public CraftingGuide() {
 		
+		Modele modl = new Modele();
+		Iterator<Item> it = modl.inventaireCreatif.research("Iron".toLowerCase()).iterator();
+		while(it.hasNext()) {
+			Item item = it.next();
+			System.out.println(item.getName());
+		}
 	}
 	
 	public static void main(String[] args) {
