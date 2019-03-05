4db6f2a9b032dac4d9e2a7052f3f6e6c12ca67fd
Sylvain81452 <47383150+Sylvain81452@users.noreply.github.com>
2019-03-05 14:34:54 +0100

    Ajout de differents modèles de tables de craft

--- src/IHM/Main.java
+++ src/IHM/Main.java
@@ -4,6 +4,8 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 
 
@@ -12,15 +14,21 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			//BorderPane root = new BorderPane();
+			
+			Image bk = new Image("file:Table_de_Craft.png");
+			ImageView mv = new ImageView(bk);
+			/*mv.setFitWidth(500);
+			mv.setFitHeight(500);*/
+			mv.setImage(bk);
 			Group root = new Group();
 			Group g2 = new Group();
-			
 			ComposantPerso c = new ComposantPerso();
 			
-			root.getChildren().addAll(g2,c);
+			root.getChildren().addAll(/*g2,c,*/mv);
 			
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Scene scene = new Scene(root,335,315);
+			
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("Crafting Guide");
 			primaryStage.setResizable(false);
 			primaryStage.setScene(scene);
