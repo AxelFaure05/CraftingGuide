bada3019cc07e7a8984d084089e42a30b76aea83
Axel <akatsumero@outlook.fr>
2019-03-06 01:19:15 +0100

    #2 - Généralisations vers objet Stack pour éviter conflits entre item et stack

--- src/Modele/Composants/ItemMatrix/ItemMatrix.java
+++ src/Modele/Composants/ItemMatrix/ItemMatrix.java
@@ -3,19 +3,25 @@ package Modele.Composants.ItemMatrix;
 import java.util.ArrayList;
 
 import Modele.Composants.Item;
+import Modele.Composants.Stack;
 
 public class ItemMatrix {
 	
 	public final static int[] size = {1, 3};
 	
-	Item[] matrix;
+	Object[] matrix;
 	
 	public ItemMatrix(int size) {
 		this.matrix = new Item[size*size];
 	}
 	
-	public void add(Item item, int pos) {
-		this.matrix[pos] = item;
+	public void add(Object item, int pos) {
+		if(item instanceof Item) {
+			this.matrix[pos] = new Stack((Item) item, 1);
+		} else if(item instanceof Stack) {
+			this.matrix[pos] = item;
+		}
+		
 	}
 	
 	public void remove(int pos) {
