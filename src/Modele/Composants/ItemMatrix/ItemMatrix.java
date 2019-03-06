48d3a0ba0a5d47c4363eca6f00114e68f9ac2096
Axel <akatsumero@outlook.fr>
2019-03-06 01:00:33 +0100

    #1 - Implantation sous forme de tableau unidimensionnel d'items

--- src/Modele/Composants/ItemMatrix/ItemMatrix.java
+++ src/Modele/Composants/ItemMatrix/ItemMatrix.java
@@ -1,5 +1,24 @@
 package Modele.Composants.ItemMatrix;
 
-public class ItemMatrix {
+import java.util.ArrayList;
 
+import Modele.Composants.Item;
+
+public class ItemMatrix {
+	
+	public final static int[] size = {1, 3};
+	
+	Item[] matrix;
+	
+	public ItemMatrix(int size) {
+		this.matrix = new Item[size*size];
+	}
+	
+	public void add(Item item, int pos) {
+		this.matrix[pos] = item;
+	}
+	
+	public void remove(int pos) {
+		this.matrix[pos] = null;
+	}
 }
