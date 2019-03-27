1aca856b209d90f6e50d1cc620be0185be9bb9e1
Sylvain81452 <47383150+Sylvain81452@users.noreply.github.com>
2019-03-27 13:55:23 +0100

    Update

---
+++ src/TestIHM/Controller.java
@@ -0,0 +1,5 @@
+package TestIHM;
+
+public class Controller {
+
+}
--- src/TestIHM/Main.java
+++ src/TestIHM/Main.java
@@ -1,59 +1,25 @@
 package TestIHM;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.image.WritableImage;
-import javafx.scene.input.ClipboardContent;
-import javafx.scene.input.DataFormat;
-import javafx.scene.input.Dragboard;
-import javafx.scene.input.TransferMode;
-import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-import javafx.scene.paint.Paint;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
-public class Main extends Application { 
-	  
-    @Override 
-    public void start(Stage primaryStage) { 
-        final Rectangle rectangle = new Rectangle(50, 50, 150, 100); 
-        rectangle.setFill(Color.RED); 
-        rectangle.setOnDragDetected(mouseEvent -> { 
-            System.out.println("DnD detecté."); 
-            final Dragboard dragBroard = rectangle.startDragAndDrop(TransferMode.COPY); 
-            // Remlissage du contenu. 
-            final ClipboardContent content = new ClipboardContent(); 
-            // Exporter en tant que texte. 
-            content.putString("Un rectangle rouge."); 
-            // Exporter en tant que couleur ARGB. 
-            DataFormat paintFormat = DataFormat.lookupMimeType(Paint.class.getName()); 
-            paintFormat = (paintFormat == null) ? new DataFormat(Paint.class.getName()) : paintFormat; 
-            final Color color = (Color) rectangle.getFill(); 
-            final int red = (int) (255 * color.getRed()); 
-            final int green = (int) (255 * color.getGreen()); 
-            final int blue = (int) (255 * color.getBlue()); 
-            final int alpha = (int) (255 * color.getOpacity()); 
-            final int argb = alpha << 24 | red << 16 | green << 8 | blue << 0; 
-            content.put(paintFormat, argb); 
-            // Exporter en tant qu'image.        
-            final WritableImage capture = rectangle.snapshot(null, null); 
-            content.putImage(capture); 
-            // 
-            dragBroard.setContent(content); 
-            mouseEvent.consume();         
-        }); 
-        final Pane root = new Pane(); 
-        root.getChildren().setAll(rectangle); 
-        final Scene scene = new Scene(root, 600, 600); 
-        primaryStage.setTitle("Test de DnD"); 
-        primaryStage.setScene(scene); 
-        primaryStage.show(); 
-    } 
-  
-    public static void main(String[] args) { 
-        launch(args); 
-    }
+public class Main extends Application{
 
- 
+	public static void main(String args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		// TODO Auto-generated method stub
+		Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
+		primaryStage.setTitle("Hello World");
+		//primaryStage.setScene(new Scene(root,300,275));
+		primaryStage.show();
+		
+	}
+
 }
