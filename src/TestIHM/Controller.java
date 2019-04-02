c7e53ed72e4f5d47266f5aa2dfc9ea5181f7b05d
James Hawken <james_1999@live.fr>
2019-04-02 23:59:13 +0200

    mise en place des images d'items dans l'inventaire

--- src/TestIHM/Controller.java
+++ src/TestIHM/Controller.java
@@ -1,5 +1,30 @@
 package TestIHM;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
 public class Controller {
+
+    @FXML
+    private AnchorPane result;
+
+    @FXML
+    private Pane paneprincipal;
+
+    @FXML
+    private GridPane table;
+
+    @FXML
+    void dragdetect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dragdone(ActionEvent event) {
+
+    }
 
 }
--- src/TestIHM/Main.java
+++ src/TestIHM/Main.java
@@ -4,11 +4,16 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-
+	private BorderPane layout;
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
@@ -19,10 +24,13 @@ public class Main extends Application{
 		Parent root = FXMLLoader.load(getClass().getResource("interface.fxml"));
         root.setId("fenetre");
 		Scene scene = new Scene(root);
+	
 		//scene.getStylesheets().addAll(this.getClass().getResource("application.css").toExternalForm());
 		scene.getStylesheets().add("application.css");
 		
+		
 		primaryStage.setTitle("Hello World");
+		primaryStage.setScene(scene);
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		
