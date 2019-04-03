e6a8e501c1c07dd421d452ced68dc336370ebe4d
Sylvain81452 <47383150+Sylvain81452@users.noreply.github.com>
2019-04-03 15:07:36 +0200

    Update Controller.java

--- src/TestIHM/Controller.java
+++ src/TestIHM/Controller.java
@@ -38,13 +38,6 @@ public class Controller {
     }
     
     @FXML
-    private void mouseEntered(MouseEvent e) {
-        Node source = (Node)e.getSource() ;
-        Integer colIndex = GridPane.getColumnIndex(source);
-        Integer rowIndex = GridPane.getRowIndex(source);
-        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
-    }
-    
     public void coords(MouseEvent e) {
     	System.out.println("click dans l'inventaire");
 		Node source = (Node)e.getSource() ;
