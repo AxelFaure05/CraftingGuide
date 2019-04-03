e9cbf32d4522bb5dbb3ab43a5f7909490c5f7ba1
Sylvain81452 <47383150+Sylvain81452@users.noreply.github.com>
2019-04-03 11:58:24 +0200

    Update TestIHM

---
+++ Controller.java
@@ -0,0 +1,45 @@
+package TestIHM;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class Controller {
+
+    @FXML
+    private AnchorPane result;
+
+    @FXML
+    private Pane paneprincipal;
+
+    @FXML
+    private GridPane table;
+
+    @FXML
+    void dragdetect(ActionEvent event) {
+
+    }
+
+    @FXML
+    void dragdone(ActionEvent event) {
+
+    }
+    
+    public GridPane returnGridPane() {
+    	return this.table;
+    }
+    
+
+    @FXML
+    private void mouseEntered(MouseEvent e) {
+        Node source = (Node)e.getSource() ;
+        Integer colIndex = GridPane.getColumnIndex(source);
+        Integer rowIndex = GridPane.getRowIndex(source);
+        System.out.printf("Mouse entered cell [%d, %d]%n", colIndex.intValue(), rowIndex.intValue());
+    }
+    
+}
