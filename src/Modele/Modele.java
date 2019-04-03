f2c103d438ea764229f85275b59a020f4d125511
Axel Faure <afaure05@pcalb-mm0000.univ-jfc.fr>
2019-04-03 11:51:24 +0200

    Implantation de la serialisation

--- src/Modele/Modele.java
+++ src/Modele/Modele.java
@@ -1,5 +1,11 @@
 package Modele;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Observable;
 
 import Modele.Composants.CraftList;
@@ -8,7 +14,10 @@ import Modele.Composants.ItemList.ItemList;
 import Modele.Composants.ItemMatrix.ItemMatrix;
 import javafx.scene.Scene;
 
-public class Modele extends Observable {
+public class Modele extends Observable implements Serializable {
+	
+	public final static String DATA = "./Data/DAT_files/DATA.dat";
+	public static int[] dim = {2, 9};
 	
 	public ItemList inventaireCreatif;
 	public Inventaire inventaireSurvie;
@@ -19,11 +28,17 @@ public class Modele extends Observable {
 	public Scene onglet;
 	
 	public Modele() {
-		this.inventaireCreatif = new ItemList("src/Modele/listeCompleteDesItems.txt");
-		this.inventaireSurvie = new Inventaire(3, 9);
-		this.craftingTable = new ItemMatrix(3);
-		this.resultatCraft = new ItemMatrix(1);
-		this.cL = new CraftList("src/Modele/listeCompleteDesCrafts.txt", this.inventaireCreatif);
+		
+		//try {
+			//this.deserialize();
+			
+		//} catch(Exception e) {
+			this.inventaireCreatif = new ItemList("src/Modele/listeCompleteDesItems.txt");
+			this.inventaireSurvie = new Inventaire(3, 9);
+			this.craftingTable = new ItemMatrix(3);
+			this.resultatCraft = new ItemMatrix(1);
+			this.cL = new CraftList("src/Modele/listeCompleteDesCrafts.txt", this.inventaireCreatif);
+		//}
 	}
 	
 	public void changedScene(Scene scn) {
@@ -46,7 +61,28 @@ public class Modele extends Observable {
 		
 	}
 	
-	public void craf() {
+	public void craft() {
 		
+	}
+	public void serialize(String str) {
+		try {
+			FileOutputStream fos = new FileOutputStream(new File(str));
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(this);
+			oos.close();
+			fos.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void deserialize() {
+		try {
+			this.inventaireCreatif = (new ItemList()).deserialize(ItemList.EMPLACEMENT_LISTECOMPLETE);
+			this.inventaireSurvie = (new Inventaire(dim[0], dim[1])).deserialize();
+			this.cL = (new CraftList()).deserialize();
+		} catch (Exception e) {
+			
+		}
 	}
 }
