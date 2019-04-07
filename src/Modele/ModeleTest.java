83eed9e9bd976c3e344d2f425ea2f67d3c273c0b
AxelFaure05 <akatsumero@outlook.fr>
2019-04-07 19:28:00 +0200

    Tests unitaires des méthodes du modèle

--- src/Modele/ModeleTest.java
+++ src/Modele/ModeleTest.java
@@ -9,28 +9,44 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Modele.Composants.Item;
+import Modele.Composants.ItemMatrix.ItemMatrix;
 
 class ModeleTest {
 	
 	Modele modl;
+	Item itm1;
+	Item itm2;
 
 	@BeforeEach
 	void setUp() throws Exception {
 		modl = new Modele();
+		itm1 = modl.inventaireCreatif.research("furnace", true).racine();
+		itm2 = modl.inventaireCreatif.research("cobblestone", true).racine();
 	}
 
 	@Test
-	void test() {
-		Item item = modl.inventaireCreatif.research("furnace").racine();
-		modl.itemToUncraft.getMatrix()[0] = item;
-
-		System.out.println(modl.itemToUncraft);
-		System.out.println(Arrays.toString(modl.itemToUncraft.getMatrix()));
-		System.out.println(item.getName());
-		System.out.println(Arrays.toString(modl.cL.keySet().toArray()));
-		System.out.println(Arrays.toString(modl.cL.get(item.getName()).getMatrix()));
+	void testUncraft() {
+		modl.itemToUncraft.getMatrix()[0] = itm1;
 		modl.uncraft();
 		
-		System.out.println(Arrays.toString(modl.uncraftResult.getMatrix()));
+		assertEquals(modl.cL.get(itm1.getName()).getMatrix().equals(modl.uncraftResult.getMatrix()), true);
+	}
+	
+	@Test
+	void testCraft() {
+		ItemMatrix im = new ItemMatrix(ItemMatrix.size[1]);
+		Object[] matrix = im.getMatrix();
+		
+		for(int k = 0; k<matrix.length; k++) {
+			if(k != 4) {
+				matrix[k] = itm2;
+			} else {
+				matrix[k] = new Item(0, "vide", "img_vide.png", false, false, false);
+			}
+		}
+		modl.craftingTable.setMatrix(matrix);
+		modl.craft();
+		
+		assertEquals(((Item) modl.resultatCraft.getMatrix()[0]).equals((Item) itm1), true);
 	}
 }
