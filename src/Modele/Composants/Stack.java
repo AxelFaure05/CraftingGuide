c56d1cb89a7b51a12b3f82827098c91d0181dd7f
AxelFaure05 <47383115+AxelFaure05@users.noreply.github.com>
2019-04-14 15:57:01 +0200

    Suppressions
    #1 - classe à refaire pour prendre en compte les exceptions possibles

--- src/Modele/Composants/Stack.java
+++ src/Modele/Composants/Stack.java
@@ -1,69 +1,32 @@
 package Modele.Composants;
 
-import Modele.Exceptions.ExceptionItemNonCorrespondant;
-import Modele.Exceptions.ExceptionItemNonStackable;
-import Modele.Exceptions.ExceptionStackOverflow;
+import java.io.Serializable;
 
-public class Stack {
+public class Stack implements Serializable {
 	
-	int count;
-	Item item;
+	private Item item;
+	private int count;
 	
 	public Stack(Item item, int count) {
-		
-		try {
-			if(!item.isStackable()) {
-				throw new ExceptionItemNonStackable();
-			} else {
-				this.setItem(item);
-				this.setCount(count);
-			}
-		} catch(ExceptionItemNonStackable e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public boolean remove(int removedCount) {
-		try {
-			if(this.count-removedCount<0) {
-				throw new RuntimeException("Tried to remove more item than there is");
-			} else {
-				this.count -= removedCount;
-				return true;
-			}
-		} catch(Exception e) {
-			e.printStackTrace();
-			return false;
-		}	
+		this.setItem(item);
+		this.setCount(count);
 	}
 
-	public boolean add(Item item, int addedCount) {
-		try {
-			if(this.item!=item) {
-				throw new ExceptionItemNonCorrespondant();
-			} else if(this.count==64) {
-				throw new ExceptionStackOverflow();
-			} else {
-				this.count+=addedCount;
-				return true;
-			}
-			
-		} catch(Exception e) {
-			e.printStackTrace();
-			return false;
-		}
-	}
-	
-	public int getCount() {
-		return count;
-	}
-	public void setCount(int count) {
-		this.count = count;
-	}
 	public Item getItem() {
 		return item;
 	}
 	public void setItem(Item item) {
 		this.item = item;
 	}
+	public int getCount() {
+		return count;
+	}
+	public void setCount(int count) {
+		this.count = count;
+	}
+	@Override
+	public int hashCode() {
+		return this.item.hashCode();
+	}
 }
+
