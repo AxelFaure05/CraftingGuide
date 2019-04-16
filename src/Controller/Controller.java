a429b677ba13801cc79b80a3683e9f55e2a6289c
Max <maximalmaximus@gmail.com>
2019-04-16 16:00:39 +0200

    Controller
    Fix des placement d'item dans inventaire et  table de craft

--- src/Controller/Controller.java
+++ src/Controller/Controller.java
@@ -32,56 +32,58 @@ public class Controller implements ActionListener,ItemListener, Observer {
 	public Controller(Modele m, ControllerIHM c) {
 		this.model = m;
 		this.cIHM = c;
-		Item testI = new Item(10,"objet","objet",true,true,true);
+		Item testI = new Item(10,"objet","objet",true);
 		Stack test = new Stack(testI,1);
-		model.inventaireSurvie.place(2, test);
-		model.inventaireSurvie.place(3, test);
+		model.inventaireSurvie.set(2, test);
+		model.inventaireSurvie.set(3, test);
 	}
 
 	public void detectClick(MouseEvent e) {
+
 		//Je regarde si on clique dans une pane pour debuter
 		if(e.getSource() instanceof Pane) {
-
 			Node source=(Node) e.getSource();
 			String id = source.getId().toString();
 			CouplePerso coord = cIHM.coords(e);
-			Integer position = cIHM.coordsToPosition(coord.x, coord.y);
 
 			//J'identifie le click de sourie
 			if(e.getButton() == MouseButton.PRIMARY) {
 
 
 
-
 				//J'identifie si nous cliquons dans l'inventaire
 				if(id.equals("inventory")||id.equals("inventory1")||id.equals("inventory2")) {
-
+					Integer position = cIHM.coordsToPosition(coord.x, coord.y)-1;
 					//Je recupere les infos du stack d'item clique
-					Stack stackRecup = model.inventaireSurvie.getInv()[position];
-
+					Stack stackRecup = model.inventaireSurvie.get(position);
+					System.out.println(position);
 					//Je regarde si j'ai deja un item dans la main oui ou non
 					if(!itemEnMain) {
 
 						//Si j'ai pas d'item dans la main, et que je clique sur une case ou se trouve un item, je la recupere
 						if(stackRecup != null) {
-							//System.out.println("je recupere l'item");
-							stackTemp = model.inventaireSurvie.getInv()[position];
-							model.inventaireSurvie.remove(position);
+							System.out.print("je recupere l'item ");
+							System.out.println(position);
+							stackTemp = model.inventaireSurvie.get(position);
+							model.inventaireSurvie.set(position, null);
 							itemEnMain = !itemEnMain;
 						}
 					}
 					else {
 						//Si j'ai un item dans la main, et que la case clique est vide, alors je pose l'item dans cette case de l'inventaire
 						if(stackRecup == null) {
-							//System.out.println("je pose l'item");
-							model.inventaireSurvie.place(position, stackTemp);
+							System.out.print("je pose l'item ");
+							System.out.println(position);
+
+							model.inventaireSurvie.set(position, stackTemp);
 							itemEnMain=!itemEnMain;
 						}
 						//Si la case clique n'est pas vide, alors j'echange les deux items de place
 						else {
-							//System.out.println("jechange deux items de place");
-							model.inventaireSurvie.remove(position);
-							model.inventaireSurvie.place(position, stackTemp);
+							System.out.print("jechange deux items de place ");
+							System.out.println(position);
+							model.inventaireSurvie.set(position,null);
+							model.inventaireSurvie.set(position, stackTemp);
 							stackTemp = stackRecup;
 						}
 					}
@@ -91,17 +93,19 @@ public class Controller implements ActionListener,ItemListener, Observer {
 
 
 				//Je regarde si on clique sur la table de craft
-				/*if(id.equals("table")) {
-
+				if(id.equals("table")) {
+					
+					Integer position = cIHM.coordsInTable(coord.x, coord.y)-1;
+					System.out.println(position);
 					//Je recupere les infos du stack d'item clique
-					Stack stackRecup = model.craftingTable.getMatrix()[position];
+					Stack stackRecup = model.tableDeCraft.getMatrix()[position];
 
 					//Je regarde si j'ai deja un item dans la main oui ou non
 					if(!itemEnMain) {
 
 						//Si j'ai pas d'item dans la main, et que je clique sur une case ou se trouve un item, je la recupere
 						if(stackRecup != null) {
-							model.craftingTable.remove(position);
+							model.tableDeCraft.add(null,position);
 							stackTemp = stackRecup;
 							itemEnMain=!itemEnMain;
 						}
@@ -109,19 +113,22 @@ public class Controller implements ActionListener,ItemListener, Observer {
 					else {
 						//Si j'ai un item dans la main, et que la case clique est vide, alors je pose l'item dans cette case du table de craft
 						if(stackRecup == null) {
-							model.craftingTable.add(stackTemp, position);
+							model.tableDeCraft.add(stackTemp, position);
 							itemEnMain=!itemEnMain;
 						}
 						//Sinon j'echange les deux items de place
 						else {
-							model.craftingTable.remove(position);
-							model.craftingTable.add(stackTemp, position);
+							model.tableDeCraft.add(null, position);;
+							model.tableDeCraft.add(stackTemp, position);
 							stackTemp = stackRecup;
 						}
 					}
-				}*/
+				}
 			}
 
+		}
+		for(int i =0;i<10;i++) {
+			System.out.println(model.inventaireSurvie.get(i));
 		}
 	}
 	@Override
