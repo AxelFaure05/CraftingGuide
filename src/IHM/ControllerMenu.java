85c705bab5bc05259b47292ef02c688a913072ec
James Hawken <james_1999@live.fr>
2019-04-29 23:41:02 +0200

    tentative de réparation du controller du menu

--- src/IHM/ControllerMenu.java
+++ src/IHM/ControllerMenu.java
@@ -1,5 +1,6 @@
 package IHM;
 
+import TestIHM.Main;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
@@ -11,7 +12,9 @@ public class ControllerMenu {
 
     @FXML
     void Démarrer(ActionEvent event) {
-
+    	System.out.println("hello");
+    	//TestIHM.Main main= new Main();
+    	
     }
 
     @FXML
